ee066547a180b407b2d61022d6cfbd76ab7c4e14
alexis-fondard <alexis.fondard@etu.umontpellier.fr>
2023-05-15 14:18:55 +0200

    firstquestions.dart updated

--- lib/views/firstquestions.dart
+++ lib/views/firstquestions.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:projet_indus/models/client.dart';
+import 'package:projet_indus/models/question.dart';
+import 'package:projet_indus/services/questionService.dart';
 import 'package:projet_indus/views/card_view.dart';
 import 'package:swipeable_card_stack/swipeable_card_stack.dart';
-import 'dart:math';
 
 class FirstQuestions extends StatefulWidget {
   const FirstQuestions({Key? key, required this.client}) : super(key: key);
@@ -14,13 +15,13 @@ class FirstQuestions extends StatefulWidget {
 }
 
 class _FirstQuestionsState extends State<FirstQuestions> {
-  int counter = 4;
-  late List<Question> questions;
+  int counter = 0;
+  late Future<List<Question>> questions;
 
   @override
   void initState() {
     super.initState();
-    questions = get10FirstQuestions();
+    questions = QuestionService().getStarters();
   }
 
   @override
@@ -32,53 +33,65 @@ class _FirstQuestionsState extends State<FirstQuestions> {
           title: const Text('Questions d\'usage'),
         ),
         body: SafeArea(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                SwipeableCardsSection(
-                  cardController: cardController,
-                  context: context,
-                  items: questions.map((question) {
-                    return CardView(
-                      text: question['label'],
-                      choice0: question['choix0'],
-                      choice1: question['choix1'],
-                      progress: counter / 10.0,
-                    );
-                  }).toList(),
-                  onCardSwiped: (dir, index, widget) {
-                    if (counter <= 20) {
-                      counter++;
-                    }
-
-                    if (dir == Direction.left) {
-                      print('onDisliked ${(widget as CardView).text} $index');
-                    } else if (dir == Direction.right) {
-                      print('onLiked ${(widget as CardView).text} $index');
-                    }
-                  },
-                  enableSwipeUp: false,
-                  enableSwipeDown: false,
-                ),
-                Container(
-                  margin: const EdgeInsets.symmetric(vertical: 20.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            child: FutureBuilder<List<Question>>(
+              future: questions,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const CircularProgressIndicator();
+                } else if (snapshot.hasError) {
+                  return Text('Error: ${snapshot.error}');
+                } else {
+                  return Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      FloatingActionButton(
-                        child: const Icon(Icons.chevron_left),
-                        onPressed: () => cardController.triggerSwipeLeft(),
+                      SwipeableCardsSection(
+                        cardController: cardController,
+                        context: context,
+                        items: snapshot.data!.map((question) {
+                          return CardView(
+                            text: question.label,
+                            choice0: question.choice0,
+                            choice1: question.choice1,
+                            progress: counter / 10.0,
+                          );
+                        }).toList(),
+                        onCardSwiped: (dir, index, widget) {
+                          if (counter <= 10) {
+                            counter++;
+                          }
+
+                          if (dir == Direction.left) {
+                            print('onDisliked ${(widget as CardView).text} $index');
+                          } else if (dir == Direction.right) {
+                            print('onLiked ${(widget as CardView).text} $index');
+                          }
+                        },
+                        enableSwipeUp: false,
+                        enableSwipeDown: false,
                       ),
-                      FloatingActionButton(
-                        child: const Icon(Icons.chevron_right),
-                        onPressed: () => cardController.triggerSwipeRight(),
-                      ),
+                      Container(
+                        margin: const EdgeInsets.symmetric(vertical: 20.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            FloatingActionButton(
+                              child: const Icon(Icons.chevron_left),
+                              onPressed: () => cardController.triggerSwipeLeft(),
+                            ),
+                            FloatingActionButton(
+                              child: const Icon(Icons.chevron_right),
+                              onPressed: () => cardController.triggerSwipeRight(),
+                            ),
+                          ],
+                        ),
+                      )
                     ],
-                  ),
-                )
-              ],
+                  );
+                }
+              },
             )
         )
     );
   }
 }
+
