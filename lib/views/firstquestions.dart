910194f77632f7db7bc4edbc2bd302115573efaa
alexis-fondard <alexis.fondard@etu.umontpellier.fr>
2023-05-15 14:02:15 +0200

    Auto stash before merge of "main" and "origin/main"

--- lib/views/firstquestions.dart
+++ lib/views/firstquestions.dart
@@ -1,43 +1,84 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-
 import 'package:projet_indus/models/client.dart';
 import 'package:projet_indus/views/card_view.dart';
+import 'package:swipeable_card_stack/swipeable_card_stack.dart';
+import 'dart:math';
 
 class FirstQuestions extends StatefulWidget {
-  const FirstQuestions({super.key, required this.client});
+  const FirstQuestions({Key? key, required this.client}) : super(key: key);
 
   final Client client;
+
   @override
   State<FirstQuestions> createState() => _FirstQuestionsState();
 }
 
 class _FirstQuestionsState extends State<FirstQuestions> {
+  int counter = 4;
+  late List<Question> questions;
+
+  @override
+  void initState() {
+    super.initState();
+    questions = get10FirstQuestions();
+  }
+
   @override
   Widget build(BuildContext context) {
-    print(widget.client.email);
+    SwipeableCardSectionController cardController = SwipeableCardSectionController();
+
     return Scaffold(
+        appBar: AppBar(
+          title: const Text('Questions d\'usage'),
+        ),
         body: SafeArea(
             child: Column(
-      children: [
-        
-        
-        
-        /*
-        ElevatedButton(
-          onPressed: () {
-            FirebaseAuth.instance.signOut();
-          },
-          style: ElevatedButton.styleFrom(
-            backgroundColor: Colors.black,
-          ),
-          child: const Text('Sign Out MAIS LE NOUVO'),
-        ),
-        Text(widget.client.name!),
-      */
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                SwipeableCardsSection(
+                  cardController: cardController,
+                  context: context,
+                  items: questions.map((question) {
+                    return CardView(
+                      text: question['label'],
+                      choice0: question['choix0'],
+                      choice1: question['choix1'],
+                      progress: counter / 10.0,
+                    );
+                  }).toList(),
+                  onCardSwiped: (dir, index, widget) {
+                    if (counter <= 20) {
+                      counter++;
+                    }
 
-      ],
-      
-    )));
+                    if (dir == Direction.left) {
+                      print('onDisliked ${(widget as CardView).text} $index');
+                    } else if (dir == Direction.right) {
+                      print('onLiked ${(widget as CardView).text} $index');
+                    }
+                  },
+                  enableSwipeUp: false,
+                  enableSwipeDown: false,
+                ),
+                Container(
+                  margin: const EdgeInsets.symmetric(vertical: 20.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      FloatingActionButton(
+                        child: const Icon(Icons.chevron_left),
+                        onPressed: () => cardController.triggerSwipeLeft(),
+                      ),
+                      FloatingActionButton(
+                        child: const Icon(Icons.chevron_right),
+                        onPressed: () => cardController.triggerSwipeRight(),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            )
+        )
+    );
   }
 }
