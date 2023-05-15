86e617de0d76780ecf1f8aa9d3fcb26b7f37a2bf
johantorosjan <johan.torosjan@etu.umontpellier.fr>
2023-05-15 14:17:59 +0200

    DAO,services,model of question

--- lib/services/questionService.dart
+++ lib/services/questionService.dart
@@ -3,8 +3,15 @@ import 'package:projet_indus/DTOs/ratingdto.dart';
 import 'package:projet_indus/models/question.dart';
 
 class QuestionService {
-  Future<List<Question>?> getStarters() async {
-    return await QuestionDAO().getStarters();
+  Future<List<Question>> getStarters() async {
+    List<Question>? questionList = [];
+    questionList = await QuestionDAO().getStarters();
+    if (questionList != null) {
+      return questionList;
+    } else {
+      List<Question> emptyQuestionList = [];
+      return emptyQuestionList;
+    }
   }
 
   void rating(int idUtilisateur, RatingDTO ratingDTO) {
