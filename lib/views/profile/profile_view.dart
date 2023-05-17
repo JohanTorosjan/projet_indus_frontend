4975c85fa85ffc6c0842ef77092ff1598a143d00
alexis-fondard <alexis.fondard@etu.umontpellier.fr>
2023-05-17 15:10:29 +0200

    update profile view

--- lib/views/profile/profile_view.dart
+++ lib/views/profile/profile_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 import '../../models/client.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ProfileView extends StatefulWidget {
   final Client client;
@@ -33,6 +34,9 @@ class ProfileViewState extends State<ProfileView> {
 
   Future<void> _selectDate(BuildContext context) async {
     final ThemeData theme = Theme.of(context);
+    final ScaffoldMessengerState scaffoldMessenger =
+        ScaffoldMessenger.of(context);
+
     final DateTime? picked = await showDatePicker(
       context: context,
       initialDate: DateTime.now(),
@@ -42,20 +46,22 @@ class ProfileViewState extends State<ProfileView> {
         return Theme(
           data: theme.copyWith(
             colorScheme: theme.colorScheme.copyWith(
-              primary: Colors.black,
+              primary:
+                  const Color.fromARGB(255, 0, 0, 0), // Changed primary color
             ),
           ),
           child: child!,
         );
       },
     );
+
     if (picked != null) {
       final age = _calculateAge(picked);
       if (age >= 18) {
         _dobController.text = DateFormat('dd/MM/yyyy').format(picked);
       } else {
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text('Vous devez avoir au moins 18 ans')),
+        scaffoldMessenger.showSnackBar(
+          const SnackBar(content: Text('Vous devez avoir au moins 18 ans')),
         );
       }
     }
@@ -76,33 +82,156 @@ class ProfileViewState extends State<ProfileView> {
     return Dialog(
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
       child: Container(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            const CircleAvatar(
-              radius: 50.0,
-              backgroundImage: AssetImage('assets/profile_logo.png'),
-            ),
-            const SizedBox(height: 20.0),
-            Text('Name: ${client.name}'),
-            Text('Email: ${client.email}'),
-            Text('Instagram: ${client.instagram ?? "Not available"}'),
-            Text('DoB: ${client.dob}'),
-            const SizedBox(height: 20.0),
-            ElevatedButton(
-              child: const Text('Update Profile'),
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => UpdateProfileView(client: client),
-                  ),
-                );
-              },
-            ),
-          ],
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.blue.shade600, Colors.blue.shade900],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
         ),
+        padding: const EdgeInsets.all(20.0),
+        child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundColor: Colors.purple.shade900,
+                  child: const Icon(
+                    Icons.person,
+                    size: 50.0,
+                    color: Colors.white,
+                  ),
+                ),
+                const SizedBox(height: 20.0),
+                _buildTextField(_nameController, 'Nom'),
+                const SizedBox(height: 10.0),
+                _buildTextField(_emailController, 'Mail'),
+                const SizedBox(height: 10.0),
+                _buildTextField(_instaController, 'Instagram'),
+                const SizedBox(height: 10.0),
+                _buildDateField(_dobController, 'Date de naissance'),
+                const SizedBox(height: 20.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.only(right: 10.0),
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.purple.shade900,
+                          ),
+                          child: const Text('Mettre à jour'),
+                          onPressed: () {
+                            // TODO: Implement update profile functionality
+                          },
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 10.0),
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.purple.shade900,
+                          ),
+                          child: const Text('Se déconnecter'),
+                          onPressed: () {
+                            // TODO: Implement logout functionality
+                          },
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            )),
       ),
     );
+  }
+
+  Widget _buildTextField(TextEditingController controller, String labelText,
+      {bool isOutlined = false}) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(labelText, style: const TextStyle(color: Colors.grey)),
+        TextFormField(
+          controller: controller,
+          decoration: isOutlined
+              ? InputDecoration(
+                  labelText: labelText,
+                  labelStyle: const TextStyle(color: Colors.white),
+                  enabledBorder: const OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.purple.shade900),
+                  ),
+                )
+              : null,
+          validator: (value) {
+            if (value == null || value.trim().isEmpty) {
+              return '$labelText ne peut pas être vide';
+            }
+            return null;
+          },
+        ),
+      ],
+    );
+  }
+
+  Widget _buildDateField(TextEditingController controller, String label) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(label, style: const TextStyle(color: Colors.grey)),
+        GestureDetector(
+          onTap: () => _selectDate(context),
+          child: AbsorbPointer(
+            child: TextFormField(
+              controller: controller,
+              readOnly: true,
+              validator: (value) {
+                if (value == null || value.trim().isEmpty) {
+                  return '$label ne peut pas être vide';
+                }
+                return null;
+              },
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  void _saveProfile() {
+    /*if (_formKey.currentState!.validate()) {
+      // Create a new Client instance with updated data
+      Client updatedClient = Client(
+        id: widget.client.id,
+        firebase_id: widget.client.firebase_id,
+        name: _nameController.text,
+        email: _emailController.text,
+        instagram: _instaController.text,
+        dob: DateFormat('dd/MM/yyyy').parse(_dobController.text),
+        has_active_session: widget.client.has_active_session,
+        answered_questions: widget.client.answered_questions,
+      );
+
+      // Update the Firebase document
+      FirebaseFirestore.instance
+          .collection('clients')
+          .doc(widget.client.firebase_id)
+          .set(updatedClient.toJson())
+          .then((_) {
+        Navigator.of(context).pop();
+      }).catchError((error) {
+        // Handle any errors here.
+        print("Failed to update client: $error");
+      });
+    }*/
   }
 }
